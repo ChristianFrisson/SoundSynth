5c170a6e24b2c38bfa6ebdcc4102819cd3a1e1d2
Auston Baker Sterling <austonst@cs.unc.edu>
2018-10-23 21:13:31 -0400

    modal_sound protobuf files are now generated during build, so generated files don't contaminate source tree.

--- modal_sound/run_cmake.sh
+++ modal_sound/run_cmake.sh
@@ -26,4 +26,5 @@ mkdir -p build_test
 cd build_test
 
 install_prefix=$(pwd)"/bin"
-cmake .. -DProtobuf_INCLUDE_DIR=$Protobuf_include_dir -DProtobuf_LITE_LIBRARY=$Protobuf_lite_lib -DBOOST_INCLUDEDIR=$Boost_include_dir -DBOOST_LIBRARYDIR=$Boost_lib_dir -DProtobuf_PROTOC_EXECUTABLE=$Protoc_exe_dir -DProtobuf_LIBRARY=$Protobuf_lib -DEIGEN_INCLUDE_DIR=$Eigen_include_dir -DGSL_INCLUDE_DIR=$Gsl_include -DMKL_LIB=$Mkl_lib -DIOMP5_PATH=$Iomp5_path -DCMAKE_INSTALL_PREFIX=$install_prefix
+cmake .. -DPROTOBUF_INCLUDE_DIR=$Protobuf_include_dir -DPROTOBUF_LITE_LIBRARY=$Protobuf_lite_lib -DBOOST_INCLUDEDIR=$Boost_include_dir -DBOOST_LIBRARYDIR=$Boost_lib_dir -DPROTOBUF_PROTOC_EXECUTABLE=$Protoc_exe_dir -DPROTOBUF_LIBRARY=$Protobuf_lib -DEIGEN_INCLUDE_DIR=$Eigen_include_dir -DGSL_INCLUDE_DIR=$Gsl_include -DMKL_LIB=$Mkl_lib -DIOMP5_PATH=$Iomp5_path -DCMAKE_INSTALL_PREFIX=$install_prefix
+make
